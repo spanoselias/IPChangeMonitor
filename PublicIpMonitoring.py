2de5f5c0df3a2cfa6fb4503ed23e98a66c8a4af5
Elias <spanoselias@outlook.com>
2019-01-13 08:04:59 -0800

    1) Hash function check.

--- PublicIpMonitoring.py
+++ PublicIpMonitoring.py
@@ -1,6 +1,26 @@
+from os import walk
+from distutils.dir_util import copy_tree
+import checksumdir
+import shutil
 
-import requests
+print('Files Synchonization Tools is running...')
 
-print(requests.get('http://ip.42.pl/raw').text)
-print(requests.get("http://ipecho.net/plain?").text)
+mypath = 'C:\\Users\\Elias\\Pictures\\'
+destPath = 'C:\\Users\\Elias\\Desktop\\testBackUp'
+# hash = checksumdir.dirhash(mypath)
 
+# print(hash)
+
+shutil.make_archive('C:\\Users\\Elias\\Desktop\\testBackUp\\testing', 'zip', mypath)
+shutil.unpack_archive(str('C:\\Users\\Elias\\Desktop\\testBackUp\\testing.zip'), str(destPath), 'zip')
+
+print('Finished...')
+
+# f = []
+# for (dirpath, dirnames, filenames) in walk(mypath):
+#     f.extend(filenames)
+#     break
+#
+#     print(f)
+
+# copy_tree(mypath, destPath)
